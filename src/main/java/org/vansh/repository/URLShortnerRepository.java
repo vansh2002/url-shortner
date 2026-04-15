50d2c6bd1a88c9516c2661fd3f4f00782652f9b3
vansh2002 <vanshchaudhary6993@gmail.com>
2026-04-15 17:49:00 +0530

    Use Concurrent Hashmap instead of Hashmap.

--- src/main/java/org/vansh/repository/URLShortnerRepository.java
+++ src/main/java/org/vansh/repository/URLShortnerRepository.java
@@ -1,11 +1,12 @@
 package org.vansh.repository;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class URLShortnerRepository {
     private long ID = 1;
-    private final Map<String, String> shortToLong = new HashMap<>();
+    private final Map<String, String> shortToLong = new ConcurrentHashMap<>();
+    private final Map<String, String> longToShort = new ConcurrentHashMap<>();
 
     public synchronized long getNextId() {
         return ID++;
